b0ea767afe54b7354d2320850eb7a9e9b6d7e94b
Purecactus <zaha23ct@student.ju.se>
2025-09-08 15:14:20 +0200

    Changed artpiece to a simpler one
    I decided that the artpiece that I had originally chosen was too complex for now and I wasn't happy with my result. I chose a different one, consisting of simple shapes, however, it includes randomness in both jitter and angles in order to create the artpiece.

--- experiments/week1.js
+++ experiments/week1.js
@@ -1,71 +1,48 @@
-/* This is a recreation of the artwork "https://joshuadavis.com/080-Infinite-Pressure",
-In my perception, the art piece looks like columns of cirlces, 
-with randomness deciding the widht and shape of the columns. */
+/* For this recreation, I have used the lecture example: 
+https://codepen.io/pixelkind/pen/WNLraMw */
 
-const W = 500;
-const H = 500;
+const size = 22;
+const columns = 12;
+const rows = 22;
+const gap = 0;
 
-const size = 30; // The avarege size of a circle
-const gap = 8; // distance between the cirlces
-const columnAmount = 7; //number of columns
-const wobble = Math.random(); // random cirlce shape
-const colourPalette = ["#19232e", "#c9d6df", "#f2efe9", "#d6b588"]; // Colours of the columns
-const bottomMargin = 30;
+const chaos = 60; // max rotation at the very bottom row
+const jitter = size * 0.15; // position chaos
 
 function setup() {
-  createCanvas(W, H);
+  createCanvas(innerWidth, innerHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
   noLoop();
 }
 
-/* Got inspiration for the gradient from this youtube video:
-https://youtu.be/-MUOweQ6wac?si=DJdOJcCxr5d3lKMT */
 function draw() {
-  drawGradient(color("#ffffff"), color("red"));
-  /* Here I center everything in the middle 
-  and space the columns evenly across the canvas*/
-  for (let i = 0; i < columnAmount; i++) {
-    /* I'm not exactly sure how this works, but ChatGPT told me that I could use "map" in order to remap a number like this:
-    map(value, start1, stop1, start2, stop2, [withinBounds]), and I used this source: https://p5js.org/reference/p5/map */
-    const x = map(i + 0.5, 0, columnAmount, 70, W - 70) + random(-15, 15);
-    const top = H * 0.1; // starting the column 10% down from the top
-    const maxHeight = H - bottomMargin - top;
-    const h = random(100, maxHeight); // Line 29 and 30 sets a margin at the bottom of the canvas
-    drawColumn(x, top, h);
-  }
-}
+  background("#ffffff");
+  noFill();
+  stroke(0);
+  strokeWeight(1);
 
-// Making the background gradient
-function drawGradient(topCol, botCol) {
-  for (let y = 0; y < H; y++) {
-    stroke(lerpColor(topCol, botCol, y / (H - 1)));
-    line(0, y, W, y);
-  }
-}
+  // Starting to draw at the center position
+  let y = (height - size * rows - gap * (rows - 1)) / 2 + size / 2;
 
-// Drawing the columns made of cirlces
-function drawColumn(cx, top, heightPx) {
-  noStroke();
+  for (let i = 0; i < rows; i++) {
+    let x = (width - size * columns - gap * (columns - 1)) / 2 + size / 2;
 
-  const c1 = color(random(colourPalette));
-  const c2 = color(random(colourPalette));
+    const t = (i + 1) / rows;
 
-  // This line ensures that all columns has a different amount of perlin noise
-  const nOff = random(1000);
+    for (let k = 0; k < columns; k++) {
+      const dx = random(-jitter * t, jitter * t);
+      const dy = random(-jitter * t, jitter * t);
+      const angle = random(-chaos * t, chaos * t);
 
-  /* This works in the way that a column is looped from "top" to "top + heightPx" in steps of "gap"
-  at each step, an ellipse is drawn so that each column is a stack of ellipses.
-  "gap" defines the amount of ellipses */
-  for (let y = top; y < top + heightPx; y += gap) {
-    const t = (y - top) / heightPx;
-    const baseProfile = sin(t * PI); // This makes the column thin at the top and bottom and bulge in the middle
-    const noisy = noise(nOff + t * 2.0); //nOff makes each columns noise different, using Perlin noise
-    const profile = lerp(baseProfile, noisy, wobble); // wobble = 0 gives a smooth column whilst wobble = 1 gives a noisy column
+      push();
+      translate(x + dx, y + dy);
+      rotate(angle);
+      square(0, 0, size);
+      pop();
 
-    const r = size * (0.5 + profile * 0.8); // circle width
-    const ry = r * 0.7; // circle height
-
-    // Together with line 47 and 48, this chooses 2 colours from my palette at random and creates a gradient for the column
-    fill(lerpColor(c1, c2, t));
-    ellipse(cx, y, r * 2, ry);
+      x += size + gap;
+    }
+    y += size + gap;
   }
 }
