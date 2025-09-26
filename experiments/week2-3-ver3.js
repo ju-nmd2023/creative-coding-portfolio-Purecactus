40c093e438c466061fe4d2015a099134154b8a3f
Purecactus <zaha23ct@student.ju.se>
2025-09-26 08:54:04 +0200

    Now the bouncy balls finally work, with the help of a youtube video

--- experiments/week2-3-ver3.js
+++ experiments/week2-3-ver3.js
@@ -8,17 +8,21 @@ const palette = [
   "#6d4c41", // Brown
 ];
 
+// Class for cirlces bouncing around the canvas, represents one moving circle
 class Ball {
   constructor(x, y, r = 16) {
-    this.position = createVector(x, y);
-    const a = random(TWO_PI);
-    const s = random(2, 5);
-    this.velocity = createVector(cos(a) * s, sin(a) * s);
+    this.position = createVector(x, y); // Circle center
+    const angle = random(TWO_PI); // Random angle picker
+    const speed = random(2, 5); // Random speed for ball
+    this.velocity = createVector(cos(angle) * speed, sin(angle) * speed);
     this.r = r;
-    this.column = color(random(palette));
+    this.color = color(random(palette));
   }
 
+  /* Function that moces tha ball around the canvas
+   I used this thread as help: https://stackoverflow.com/questions/71803762/bouncing-an-object-off-a-wall?*/
   update() {
+    // Left and right walls
     this.position.add(this.velocity);
     if (this.position.x - this.r < 0) {
       this.position.x = this.r;
@@ -27,6 +31,8 @@ class Ball {
       this.position.x = width - this.r;
       this.velocity.x *= -1;
     }
+
+    // Top and bottom walls
     if (this.position.y - this.r < 0) {
       this.position.y = this.r;
       this.velocity.y *= -1;
@@ -38,7 +44,7 @@ class Ball {
 
   draw() {
     noStroke();
-    fill(this.column);
+    fill(this.color);
     circle(this.position.x, this.position.y, this.r * 2);
   }
 }
@@ -53,21 +59,24 @@ function setup() {
 function draw() {
   background("#f8f5ee");
 
-  // This updates the positions
+  // This updates the positions (basically moves each ball and checks for collision)
   for (const b of balls) b.update();
 
-  // This resolves the collisions between circle and circle
+  // This checks the collisions between circle and circle
   resolveCollisions(balls);
 
   for (const b of balls) b.draw();
 }
 
+// This adds a ball if the mouse button is clicked
 function mouseClicked() {
   if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) return;
   balls.push(new Ball(mouseX, mouseY, random(12, 24)));
 }
 
-//Collision detection
+/* Collision detection (Compares each and every ball to one another)
+This function is inspired from this video: https://www.youtube.com/watch?v=y14SpHKL1gg
+Along with my collision detection from the p5.js game from previous course with Garrit */
 function resolveCollisions(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
@@ -77,30 +86,35 @@ function resolveCollisions(arr) {
       let distance = delta.mag();
       const minDistance = A.r + B.r;
 
+      // Checking if circles are overlapping
+      // Random direction when cirlces overlap as to not cause errors
       if (distance === 0) {
-        // Random direction when cirlce is spawned
         delta.set(random(-1, 1), random(-1, 1));
         distance = delta.mag();
       }
 
+      // Pushing circles apart when they overlap
       if (distance < minDistance) {
         const n = delta.copy().div(distance);
         const overlap = minDistance - distance;
 
+        // This pushes the circles apart
         const correct = n.copy().mult(overlap / 2);
         A.position.sub(correct);
         B.position.add(correct);
 
-        const va = A.velocity.copy();
-        const vb = B.velocity.copy();
+        // This saves their velocity
+        const velocityA = A.velocity.copy();
+        const velocityB = B.velocity.copy();
 
-        const vaN = p5.Vector.mult(n, va.dot(n));
-        const vbN = p5.Vector.mult(n, vb.dot(n));
-        const vaT = p5.Vector.sub(va, vaN);
-        const vbT = p5.Vector.sub(vb, vbN);
+        // Rest of the code makes the bounce elastic
+        const velocityANormal = p5.Vector.mult(n, velocityA.dot(n));
+        const velocityBNormal = p5.Vector.mult(n, velocityB.dot(n));
+        const velocityATangent = p5.Vector.sub(velocityA, velocityANormal);
+        const velocityBTangent = p5.Vector.sub(velocityB, velocityBNormal);
 
-        A.velocity = p5.Vector.add(vaT, vbN);
-        B.velocity = p5.Vector.add(vbT, vaN);
+        A.velocity = p5.Vector.add(velocityATangent, velocityBNormal);
+        B.velocity = p5.Vector.add(velocityBTangent, velocityANormal);
       }
     }
   }
--- experiments/week4.js
+++ experiments/week4.js
@@ -1,111 +1,125 @@
-class Agent {
-  constructor(x, y, maxSpeed, maxForce) {
-    this.position = createVector(x, y);
-    this.lastPosition = createVector(x, y);
-    this.acceleration = createVector(0, 0);
-    this.velocity = createVector(0, 0);
-    this.maxSpeed = maxSpeed;
-    this.maxForce = maxForce;
+const palette = [
+  "#e63946", // Red
+  "#f1c40f", // Yellow
+  "#2a9d8f", // Light blue/teal
+  "#264653", // Dark blue
+  "#1583c2", // Normal blue
+  "#ef8354", // Orange
+  "#6d4c41", // Brown
+];
+
+// Class for cirlces bouncing around the canvas, represents one moving circle
+class Ball {
+  constructor(x, y, r = 16) {
+    this.position = createVector(x, y); // Circle center
+    const angle = random(TWO_PI); // Random angle picker
+    const speed = random(2, 5); // Random speed for ball
+    this.velocity = createVector(cos(angle) * speed, sin(angle) * speed);
+    this.r = r;
+    this.color = color(random(palette));
   }
 
-  follow(desiredDirection) {
-    desiredDirection = desiredDirection.copy();
-    desiredDirection.mult(this.maxSpeed);
-    let steer = p5.Vector.sub(desiredDirection, this.velocity);
-    steer.limit(this.maxForce);
-    this.applyForce(steer);
-  }
-
-  applyForce(force) {
-    this.acceleration.add(force);
-  }
-
+  /* Function that moces tha ball around the canvas
+   I used this thread as help: https://stackoverflow.com/questions/71803762/bouncing-an-object-off-a-wall?*/
   update() {
-    this.lastPosition = this.position.copy();
-    this.velocity.add(this.acceleration);
-    this.velocity.limit(this.maxSpeed);
+    // Left and right walls
     this.position.add(this.velocity);
-    this.acceleration.mult(0);
-  }
-
-  checkBorders() {
-    if (this.position.x < 0) {
-      this.position.x = innerWidth;
-      this.lastPosition.x = innerWidth;
-    } else if (this.position.x > innerWidth) {
-      this.position.x = 0;
-      this.lastPosition.x = 0;
+    if (this.position.x - this.r < 0) {
+      this.position.x = this.r;
+      this.velocity.x *= -1;
+    } else if (this.position.x + this.r > width) {
+      this.position.x = width - this.r;
+      this.velocity.x *= -1;
     }
-    if (this.position.y < 0) {
-      this.position.y = innerHeight;
-      this.lastPosition.y = innerHeight;
-    } else if (this.position.y > innerHeight) {
-      this.position.y = 0;
-      this.lastPosition.y = 0;
+
+    // Top and bottom walls
+    if (this.position.y - this.r < 0) {
+      this.position.y = this.r;
+      this.velocity.y *= -1;
+    } else if (this.position.y + this.r > height) {
+      this.position.y = height - this.r;
+      this.velocity.y *= -1;
     }
   }
 
   draw() {
-    push();
-    stroke(0, 0, 0, 40);
-    strokeWeight(1);
-    line(
-      this.lastPosition.x,
-      this.lastPosition.y,
-      this.position.x,
-      this.position.y
-    );
-    pop();
+    noStroke();
+    fill(this.color);
+    circle(this.position.x, this.position.y, this.r * 2);
   }
 }
+
+let balls = [];
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  background(255, 255, 255);
-  field = generateField();
-  generateAgents();
+  background("#f8f5ee");
 }
-
-function generateField() {
-  let field = [];
-  noiseSeed(Math.random() * 100);
-  for (let x = 0; x < maxCols; x++) {
-    field.push([]);
-    for (let y = 0; y < maxRows; y++) {
-      const value = noise(x / divider, y / divider) * Math.PI * 2;
-      field[x].push(p5.Vector.fromAngle(value));
-    }
-  }
-  return field;
-}
-
-function generateAgents() {
-  for (let i = 0; i < 200; i++) {
-    let agent = new Agent(
-      Math.random() * innerWidth,
-      Math.random() * innerHeight,
-      4,
-      0.1
-    );
-    agents.push(agent);
-  }
-}
-
-const fieldSize = 50;
-const maxCols = Math.ceil(innerWidth / fieldSize);
-const maxRows = Math.ceil(innerHeight / fieldSize);
-const divider = 4;
-let field;
-let agents = [];
 
 function draw() {
-  for (let agent of agents) {
-    const x = Math.floor(agent.position.x / fieldSize);
-    const y = Math.floor(agent.position.y / fieldSize);
-    const desiredDirection = field[x][y];
-    agent.follow(desiredDirection);
-    agent.update();
-    agent.checkBorders();
-    agent.draw();
+  background("#f8f5ee");
+
+  // This updates the positions (basically moves each ball and checks for collision)
+  for (const b of balls) b.update();
+
+  // This checks the collisions between circle and circle
+  resolveCollisions(balls);
+
+  for (const b of balls) b.draw();
+}
+
+// This adds a ball if the mouse button is clicked
+function mouseClicked() {
+  if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) return;
+  balls.push(new Ball(mouseX, mouseY, random(12, 24)));
+}
+
+/* Collision detection (Compares each and every ball to one another)
+This function is inspired from this video: https://www.youtube.com/watch?v=y14SpHKL1gg
+Along with my collision detection from the p5.js game from previous course with Garrit */
+function resolveCollisions(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const A = arr[i];
+      const B = arr[j];
+      let delta = p5.Vector.sub(B.position, A.position);
+      let distance = delta.mag();
+      const minDistance = A.r + B.r;
+
+      // Checking if circles are overlapping
+      // Random direction when cirlces overlap as to not cause errors
+      if (distance === 0) {
+        delta.set(random(-1, 1), random(-1, 1));
+        distance = delta.mag();
+      }
+
+      // Pushing circles apart when they overlap
+      if (distance < minDistance) {
+        const n = delta.copy().div(distance);
+        const overlap = minDistance - distance;
+
+        // This pushes the circles apart
+        const correct = n.copy().mult(overlap / 2);
+        A.position.sub(correct);
+        B.position.add(correct);
+
+        // This saves their velocity
+        const velocityA = A.velocity.copy();
+        const velocityB = B.velocity.copy();
+
+        // Rest of the code makes the bounce elastic
+        const velocityANormal = p5.Vector.mult(n, velocityA.dot(n));
+        const velocityBNormal = p5.Vector.mult(n, velocityB.dot(n));
+        const velocityATangent = p5.Vector.sub(velocityA, velocityANormal);
+        const velocityBTangent = p5.Vector.sub(velocityB, velocityBNormal);
+
+        A.velocity = p5.Vector.add(velocityATangent, velocityBNormal);
+        B.velocity = p5.Vector.add(velocityBTangent, velocityANormal);
+      }
+    }
   }
+}
+
+function windowResized() {
+  resizeCanvas(innerWidth, innerHeight);
 }
